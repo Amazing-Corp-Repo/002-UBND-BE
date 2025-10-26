5900a097bd95b80a1c9a1ca2fc44b9005a848859
Mikan382 <annguyen382310@gmail.com>
2025-10-26 22:50:27 +0700

    feat: checklist

--- src/routes/root.route.js
+++ src/routes/root.route.js
@@ -2,12 +2,14 @@ import express from 'express';
 import authRoute from './auth.route.js';
 import userRoute from './user.route.js';
 import fileRoute from './file.route.js';
+import thuTucRoute from './thu-tuc.route.js';
 
 const rootRouter = express.Router();
 
 rootRouter.use('/auths', authRoute);
 rootRouter.use('/users', userRoute);
 rootRouter.use('/files', fileRoute);
+rootRouter.use('/thu-tuc', thuTucRoute);
 
 export default rootRouter;
 
