d2c6723c5258c8eefe059a3fb1f25de0761596d5
dhgb2k5 <dhgb2k5@gmail.com>
2025-10-30 01:21:40 +0700

    feat: CRUD lĩnh vực

--- src/controllers/linh-vuc.controller.js
+++ src/controllers/linh-vuc.controller.js
@@ -7,6 +7,33 @@ const LinhVucController = {
         const linhVucs = await LinhVucService.getAll(is_removed);
         return successResponse(res, linhVucs, "Lấy danh sách lĩnh vực thành công");
     },
+
+    async create(req, res) {
+        const { ten_linh_vuc, mo_ta } = req.body;
+        const newLinhVuc = await LinhVucService.create(ten_linh_vuc, mo_ta);
+        return successResponse(res, newLinhVuc, "Tạo lĩnh vực thành công");
+    },
+
+    async update(req, res) {
+        const { id } = req.params;
+        const { ten_linh_vuc, mo_ta, is_remove } = req.body;
+        const nguoi_cap_nhap = req.payload.userId;
+        const updatedLinhVuc = await LinhVucService.update(id, ten_linh_vuc, mo_ta, is_remove, nguoi_cap_nhap);
+        return successResponse(res, updatedLinhVuc, "Cập nhật lĩnh vực thành công");
+    },
+
+    async softDelete(req, res) {
+        const { id } = req.params;
+        const nguoi_cap_nhap = req.payload.userId;
+        const result = await LinhVucService.softDelete(id, nguoi_cap_nhap);
+        return successResponse(res, result, "Xóa mềm lĩnh vực thành công");
+    },
+
+    async hardDelete(req, res) {
+        const { id } = req.params;
+        await LinhVucService.hardDelete(id);
+        return successResponse(res, null, "Xóa cứng lĩnh vực thành công");
+    },
 };
 
 export default LinhVucController;
