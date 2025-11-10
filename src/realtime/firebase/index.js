b131a5a8858def1517392fa354a8acfbe1fc6a39
Lê Nguyễn Minh Kha <lenguyenminhkha1606@gmail.com>
2025-11-10 18:56:46 +0700

    refactor firebase service account configuration to use env variables consistently

--- src/realtime/firebase/index.js
+++ src/realtime/firebase/index.js
@@ -4,15 +4,15 @@ import env from "../../config/environment.config.js";
 const serviceAccount = {
     type: env.FIREBASE_FIREBASE_TYPE,
     project_id: env.FIREBASE_PROJECT_ID,
-    private_key_id: process.env.FIREBASE_PRIVATE_KEY_ID,
-    private_key: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, "\n"),
-    client_email: process.env.FIREBASE_CLIENT_EMAIL,
-    client_id: process.env.FIREBASE_CLIENT_ID,
-    auth_uri: process.env.FIREBASE_AUTH_URI,
-    token_uri: process.env.FIREBASE_TOKEN_URI,
-    auth_provider_x509_cert_url: process.env.FIREBASE_AUTH_PROVIDER_X509_CERT_URL,
-    client_x509_cert_url: process.env.FIREBASE_CLIENT_X509_CERT_URL,
-    universe_domain: process.env.FIREBASE_UNIVERSE_DOMAIN,
+    private_key_id: env.FIREBASE_PRIVATE_KEY_ID,
+    private_key: env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n'),
+    client_email: env.FIREBASE_CLIENT_EMAIL,
+    client_id: env.FIREBASE_CLIENT_ID,
+    auth_uri: env.FIREBASE_AUTH_URI,
+    token_uri: env.FIREBASE_TOKEN_URI,
+    auth_provider_x509_cert_url: env.FIREBASE_AUTH_PROVIDER_X509_CERT_URL,
+    client_x509_cert_url: env.FIREBASE_CLIENT_X509_CERT_URL,
+    universe_domain: env.FIREBASE_UNIVERSE_DOMAIN,
 };
 
 if (!admin.apps.length) {
