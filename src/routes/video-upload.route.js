cffc4db648af1153a03b8de2f79e9e3b3d61e22a
Lê Nguyễn Minh Kha <lenguyenminhkha1606@gmail.com>
2025-11-28 01:57:25 +0700

    feat: implement rate limiting for video upload requests

--- src/routes/video-upload.route.js
+++ src/routes/video-upload.route.js
@@ -1,18 +1,37 @@
 import express from "express";
 import VideoUploadController from "../controllers/video-upload.controller.js";
-import { audit_logs } from "../middlewares/audit-logs.middleware.js";
-import { AUDIT_LOGS } from "../constants/audit-logs-action.constant.js";
 import UPLOAD_TYPE from "../constants/upload.constant.js";
 import { createUploader } from "../middlewares/upload.middleware.js";
 import validate from "../middlewares/validate.middleware.js";
 import { VideoUploadRequest } from "../validators/video-upload.validator.js";
 import { authenticate, authorize } from "../middlewares/auth.middleware.js";
 import { PERMISSION } from "../constants/permission.constant.js";
+import rateLimit from "express-rate-limit";
+import { errorResponse } from "../utils/response.util.js";
+import env from "../config/environment.config.js";
+
+const RATE_LIMIT_WINDOW_MS = parseInt(env.RATE_LIMIT_WINDOW_MS);
+const REATE_LIMIT_FOR_UPLOAD_VIDEO_MAX = parseInt(env.REATE_LIMIT_FOR_UPLOAD_VIDEO_MAX);
+
+const uploadChunkLimiter = rateLimit({
+  windowMs: RATE_LIMIT_WINDOW_MS,
+  max: REATE_LIMIT_FOR_UPLOAD_VIDEO_MAX,
+  standardHeaders: true,
+  legacyHeaders: false,
+  handler: (req, res, next) => {
+    return errorResponse(
+      res,
+      { message: "Too many upload requests, please try again later." },
+      429
+    );
+  },
+});
 
 const videoUploadRouter = express.Router();
 
 videoUploadRouter.post(
   "/upload",
+  uploadChunkLimiter,
   authenticate,
   authorize([PERMISSION.VID_PA_UPLOAD]),
   createUploader({
@@ -25,7 +44,6 @@ videoUploadRouter.post(
     isPublic: false,
   }),
   validate(VideoUploadRequest),
-  audit_logs(AUDIT_LOGS.CREATE, "video_uploads, video_upload_chunks"),
   VideoUploadController.uploadVideo
 );
 
