5ca70bf29071835feff97162beb68267fa5f147c
Lê Nguyễn Minh Kha <lenguyenminhkha1606@gmail.com>
2025-11-28 02:42:07 +0700

    fix: refactor cleanup logic to use for...of loops for better async handling

--- src/cron/cleanup-chunks.cron.js
+++ src/cron/cleanup-chunks.cron.js
@@ -8,30 +8,30 @@ export const registerCleanupCron = () => {
     "0 17 * * *",
     async () => {
       const videosToCleanup = await VideoUploadRepository.getVideoToCleanup();
-      videosToCleanup.forEach(async (video) => {
-        video.video_upload_chunks.forEach(async (chunk) => {
+
+      for (const video of videosToCleanup) {
+        for (const chunk of video.video_upload_chunks) {
           console.log(`  Chunk ID: ${chunk.id}, Path: ${chunk.path}`);
           try {
             const chunkFilePath = chunk.path;
             if (fs.pathExists(chunkFilePath)) {
               fs.remove(chunkFilePath);
+              console.log(`Đã xóa chunk: ${chunk.id}.`);
             } else {
               console.log(`Tệp chunk không tồn tại: ${chunkFilePath}`);
             }
 
-            await VideoUploadRepository.deleteChunk(chunk.path);
-            console.log(`Đã xóa chunk: ${chunk.id}.`);
+            await VideoUploadRepository.deleteChunk(chunk.id);
           } catch (error) {
             console.error(
               `Lỗi khi xóa chunk ID: ${chunk.id} - Error: ${error.message}`
             );
           }
-        });
-
+        }
         await VideoUploadRepository.updateVideoUpload(video.id, {
           status: VIDEO_STATUS.CLEANED_UP,
         });
-      });
+      }
     },
     {
       timezone: "utc",
