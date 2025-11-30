53f1c65057746b1eb96dd83e27f8337097849a23
Lê Nguyễn Minh Kha <lenguyenminhkha1606@gmail.com>
2025-11-30 22:53:17 +0700

    add view functionality for tin tuc, including model, controller, service, and route updates

--- src/repositories/tin-tuc.repository.js
+++ src/repositories/tin-tuc.repository.js
@@ -1,78 +1,146 @@
 import prisma from "../config/database.config.js";
 
 const TinTucRepository = {
-    async create(data) {
-        return prisma.tin_tuc.create({
-            data
-        });
-    },
+  async create(data) {
+    return prisma.tin_tuc.create({
+      data,
+    });
+  },
 
-    async findById(id) {
-        return prisma.tin_tuc.findFirst({
-            where: {
-                id: id,
-            }
-        });
-    },
+  async findById(id) {
+    return prisma.tin_tuc.findFirst({
+      where: {
+        id: id,
+      },
+    });
+  },
 
-    async update(id, data) {
-        return prisma.tin_tuc.update({
-            where: { id },
-            data
-        });
-    },
+  async update(id, data) {
+    return prisma.tin_tuc.update({
+      where: { id },
+      data,
+    });
+  },
 
-    async getDetails(id) {
-        return prisma.tin_tuc.findFirst({
-            where: { 
-                id,
-                is_delete: false 
-            },
-            include: {
-                dinh_kem_tin_tuc: true,
-                danh_muc_tin_tuc: true,
-            }
-        });
-    },
+  async getDetails(id) {
+    return prisma.tin_tuc.findFirst({
+      where: {
+        id,
+        is_delete: false,
+      },
+      include: {
+        dinh_kem_tin_tuc: true,
+        danh_muc_tin_tuc: true,
+        _count: {
+          select: { tin_tuc_view: true },
+        },
+      },
+    });
+  },
 
-    async getAll(page, size, idDanhMuc, isActive, search) {
-        const skip = (page - 1) * size;
-        const where = {
-            ...(isActive !== undefined && isActive !== ''
-                ? { is_active: isActive === 'true' }
-                : {}),
-            ...(idDanhMuc ? { id_danh_muc: idDanhMuc } : {}),
-            is_delete: false,
-            ...(search !== undefined && search !== ''
-                ? { tieu_de: { contains: search, mode: 'insensitive' } }
-                : {}),
+  async getAll(page, size, idDanhMuc, isActive, search) {
+    const skip = (page - 1) * size;
+    const where = {
+      ...(isActive !== undefined && isActive !== ""
+        ? { is_active: isActive === "true" }
+        : {}),
+      ...(idDanhMuc ? { id_danh_muc: idDanhMuc } : {}),
+      is_delete: false,
+      ...(search !== undefined && search !== ""
+        ? { tieu_de: { contains: search, mode: "insensitive" } }
+        : {}),
+    };
+    const [data, totalItems] = await Promise.all([
+      prisma.tin_tuc.findMany({
+        where,
+        skip,
+        take: size,
+        orderBy: {
+          thoi_gian_tao: "desc",
+        },
+        include: {
+          danh_muc_tin_tuc: true,
+          _count: { select: { tin_tuc_view: true } },
+        },
+      }),
+      prisma.tin_tuc.count({ where }),
+    ]);
+    return { data, totalItems };
+  },
 
-        }
-        const [data, totalItems] = await Promise.all([
-            prisma.tin_tuc.findMany({
-                where,
-                skip,
-                take: size,
-                orderBy: {
-                    thoi_gian_tao: 'desc'
-                },
-                include: {
-                    danh_muc_tin_tuc: true,
-                },
-            }),
-            prisma.tin_tuc.count({ where }),
-        ]);
-        return { data, totalItems }
-    },
+  async findByIdDanhMuc(id) {
+    return prisma.tin_tuc.findFirst({
+      where: {
+        id_danh_muc: id,
+        is_delete: false,
+      },
+    });
+  },
 
-    async findByIdDanhMuc(id) {
-        return prisma.tin_tuc.findFirst({
-            where: {
-                id_danh_muc: id,
-                is_delete: false,
-            }
-        });
-    },
+  async getTinTucAndUpdateViewCount(id, ip, deviceInfo) {
+    const tinTuc = await prisma.tin_tuc.findFirst({
+      where: {
+        id,
+        is_delete: false,
+        is_active: true,
+      },
+      include: {
+        dinh_kem_tin_tuc: true,
+        danh_muc_tin_tuc: true,
+        _count: {
+          select: { tin_tuc_view: true },
+        },
+      },
+    });
+    if (!tinTuc) {
+      throw new BaseError(404, "Tin tức không tồn tại hoặc không hoạt động");
+    }
+
+    const viewRecord = await prisma.tin_tuc_view.findUnique({
+      where: {
+        id_tin_tuc_ip_address_device_info: {
+          id_tin_tuc: id,
+          ip_address: ip,
+          device_info: deviceInfo,
+        },
+      },
+    });
+
+    if (!viewRecord) {
+      await prisma.tin_tuc_view.create({
+        data: {
+          id_tin_tuc: id,
+          ip_address: ip,
+          device_info: deviceInfo,
+        },
+      });
+      return tinTuc;
+    }
+
+    const now = new Date();
+    const lastViewedAt = new Date(viewRecord.last_view_at);
+    const timeDifference = (now - lastViewedAt) / (1000 * 60 * 60);
+
+    if (timeDifference < 1) {
+      return tinTuc;
+    }
+
+    await prisma.tin_tuc_view.update({
+      where: {
+        id_tin_tuc_ip_address_device_info: {
+          id_tin_tuc: id,
+          ip_address: ip,
+          device_info: deviceInfo,
+        },
+      },
+      data: {
+        last_view_at: now,
+        view_count: { increment: 1 },
+      },
+    });
+
+    return tinTuc;
+  },
 };
 
 export default TinTucRepository;
--- src/services/tin-tuc.service.js
+++ src/services/tin-tuc.service.js
@@ -6,159 +6,212 @@ import DanhMucTinTucRepository from "../repositories/danh-muc-tin-tuc.repository
 import { capitalizeWords } from "../utils/string.util.js";
 
 const TinTucService = {
-    async uploadFile(idTinTuc, file = [], currentUser) {
-        if (!file || file.length === 0) {
-            throw new BaseError(400, 'File đính kèm là bắt buộc');
-        }
-        if (!idTinTuc || idTinTuc === '') {
-            const tinTuc = await TinTucRepository.create({
-                is_active: false,
-                nguoi_tao: currentUser,
-            });
-            idTinTuc = tinTuc.id;
-        }
-        let data = {
-            id_tin_tuc: idTinTuc,
-            url_file: file[0].relativeUrl,
-            kich_thuoc_file_mb: file[0].sizeMB,
-            dinh_dang_file: file[0].mimetype,
-            nguoi_tao: currentUser,
-        };
-
-        const result = await DinhKemTinTucRepository.create(data);
-        return {
-            id_tin_tuc: result.id_tin_tuc,
-            url_file: result.url_file
-        };
-    },
-
-    async updateTinTuc(id, idDanhMuc, tieuDe, noiDung, tacGia, isActive, file = [], currentUser) {
-        if (id === null || id === undefined) {
-            throw new BaseError(400, 'ID tin tức không được để trống');
-        }
-        tieuDe = capitalizeWords(tieuDe);
-        tacGia = tacGia ? capitalizeWords(tacGia) : tacGia;
-        const existing = await TinTucRepository.findById(id);
-        if (!existing || existing.is_delete) {
-            throw new BaseError(404, 'Tin tức không tồn tại');
-        }
-
-        const existingDanhMuc = await DanhMucTinTucRepository.findById(idDanhMuc);
-        if (!existingDanhMuc || existingDanhMuc.is_active === false) {
-            throw new BaseError(404, 'Danh mục tin tức không tồn tại');
-        }
-        let data = {
-            tac_gia: tacGia,
-            id_danh_muc: idDanhMuc,
-            tieu_de: tieuDe,
-            noi_dung: noiDung,
-            is_active: isActive,
-            is_noti: existing.is_noti,
-            nguoi_cap_nhat: currentUser,
-            thoi_gian_cap_nhat: new Date().toISOString(),
-        };
-
-        if (file && file.length > 0) {
-            data.url_anh_dai_dien = file[0].relativeUrl;
-        };
-        if (existing.is_active === false && isActive === true && !existing.is_noti) {
-            const hasNewImage = data.url_anh_dai_dien && data.url_anh_dai_dien.trim() !== '';
-            const hasOldImage = existing.url_anh_dai_dien && existing.url_anh_dai_dien.trim() !== '';
-
-            if (!hasNewImage && !hasOldImage) {
-                throw new BaseError(400, 'Ảnh đại diện tin tức là bắt buộc để xuất bản tin tức');
-            }
-
-            // Gửi thông báo xuất bản tin tức
-            data.is_noti = true;
-        }
-        const result = await TinTucRepository.update(id, data);
-        return result;
-    },
-
-    async getDetails(id) {
-        if (!id === null || id === undefined) {
-            throw new BaseError(400, 'ID tin tức không được để trống');
-        }
-        const result = await TinTucRepository.getDetails(id);
-        return result;
-    },
-
-    async getAll(page, size, idDanhMuc, isActive, search) {
-        const { data, totalItems } = await TinTucRepository.getAll(page, size, idDanhMuc, isActive, search);
-        const pagination = createPagination(page, size, totalItems);
-        return { data, pagination };
-    },
-
-    async delete(id, currentUser) {
-        if (!id === null || id === undefined) {
-            throw new BaseError(400, 'ID tin tức không được để trống');
-        }
-        const existing = await TinTucRepository.findById(id);
-        if (!existing) {
-            throw new BaseError(404, 'Tin tức không tồn tại');
-        }
-        if (existing.is_active) {
-            throw new BaseError(400, 'Chỉ có tin tức đang hoạt động mới có thể xóa');
-        }
-
-        let data = {
-            is_delete: true,
-            nguoi_cap_nhat: currentUser,
-            thoi_gian_cap_nhat: new Date().toISOString(),
-        }
-
-        await TinTucRepository.update(id, data);
-    },
-
-    async createTinTuc(idDanhMuc, tieuDe, noiDung, isActive, tacGia, file, currentUser) {
-        tieuDe = capitalizeWords(tieuDe);
-        tacGia = tacGia ? capitalizeWords(tacGia) : tacGia;
-
-        const existingDanhMuc = await DanhMucTinTucRepository.findById(idDanhMuc, false);
-        if (!existingDanhMuc) {
-            throw new BaseError(404, 'Danh mục tin tức không tồn tại');
-        }
-
-        let data = {
-            id_danh_muc: idDanhMuc,
-            tieu_de: tieuDe,
-            noi_dung: noiDung,
-            is_active: isActive,
-            tac_gia: tacGia,
-            nguoi_tao: currentUser,
-        };
-
-        if (file && file.length > 0) {
-            data.url_anh_dai_dien = file[0].relativeUrl;
-        }
-        if (isActive === true) {
-            if (data.url_anh_dai_dien == null || data.url_anh_dai_dien === '') {
-                throw new BaseError(400, 'Ảnh đại diện tin tức là bắt buộc để xuất bản tin tức');
-            }
-            // Gửi thông báo xuất bản tin tức
-
-            data.is_noti = true;
-        }
-        return await TinTucRepository.create(data);
-
-    },
-
-    async updateTinTucStatus(id, isActive, currentUser) {
-        if (id === null || id === undefined) {
-            throw new BaseError(400, 'ID tin tức không được để trống');
-        }
-        const existing = await TinTucRepository.findById(id);
-        if (!existing || existing.is_delete) {
-            throw new BaseError(404, 'Tin tức không tồn tại');
-        }
-        const data = {
-            is_active: isActive,
-            nguoi_cap_nhat: currentUser,
-            thoi_gian_cap_nhat: new Date().toISOString(),
-        };
-        return await TinTucRepository.update(id, data);
+  async uploadFile(idTinTuc, file = [], currentUser) {
+    if (!file || file.length === 0) {
+      throw new BaseError(400, "File đính kèm là bắt buộc");
     }
+    if (!idTinTuc || idTinTuc === "") {
+      const tinTuc = await TinTucRepository.create({
+        is_active: false,
+        nguoi_tao: currentUser,
+      });
+      idTinTuc = tinTuc.id;
+    }
+    let data = {
+      id_tin_tuc: idTinTuc,
+      url_file: file[0].relativeUrl,
+      kich_thuoc_file_mb: file[0].sizeMB,
+      dinh_dang_file: file[0].mimetype,
+      nguoi_tao: currentUser,
+    };
+
+    const result = await DinhKemTinTucRepository.create(data);
+    return {
+      id_tin_tuc: result.id_tin_tuc,
+      url_file: result.url_file,
+    };
+  },
+
+  async updateTinTuc(
+    id,
+    idDanhMuc,
+    tieuDe,
+    noiDung,
+    tacGia,
+    isActive,
+    file = [],
+    currentUser
+  ) {
+    if (id === null || id === undefined) {
+      throw new BaseError(400, "ID tin tức không được để trống");
+    }
+    tieuDe = capitalizeWords(tieuDe);
+    tacGia = tacGia ? capitalizeWords(tacGia) : tacGia;
+    const existing = await TinTucRepository.findById(id);
+    if (!existing || existing.is_delete) {
+      throw new BaseError(404, "Tin tức không tồn tại");
+    }
+
+    const existingDanhMuc = await DanhMucTinTucRepository.findById(idDanhMuc);
+    if (!existingDanhMuc || existingDanhMuc.is_active === false) {
+      throw new BaseError(404, "Danh mục tin tức không tồn tại");
+    }
+    let data = {
+      tac_gia: tacGia,
+      id_danh_muc: idDanhMuc,
+      tieu_de: tieuDe,
+      noi_dung: noiDung,
+      is_active: isActive,
+      is_noti: existing.is_noti,
+      nguoi_cap_nhat: currentUser,
+      thoi_gian_cap_nhat: new Date().toISOString(),
+    };
+
+    if (file && file.length > 0) {
+      data.url_anh_dai_dien = file[0].relativeUrl;
+    }
+    if (
+      existing.is_active === false &&
+      isActive === true &&
+      !existing.is_noti
+    ) {
+      const hasNewImage =
+        data.url_anh_dai_dien && data.url_anh_dai_dien.trim() !== "";
+      const hasOldImage =
+        existing.url_anh_dai_dien && existing.url_anh_dai_dien.trim() !== "";
+
+      if (!hasNewImage && !hasOldImage) {
+        throw new BaseError(
+          400,
+          "Ảnh đại diện tin tức là bắt buộc để xuất bản tin tức"
+        );
+      }
+
+      // Gửi thông báo xuất bản tin tức
+      data.is_noti = true;
+    }
+    const result = await TinTucRepository.update(id, data);
+    return result;
+  },
+
+  async getDetails(id) {
+    if (!id === null || id === undefined) {
+      throw new BaseError(400, "ID tin tức không được để trống");
+    }
+    const result = await TinTucRepository.getDetails(id);
+    return result;
+  },
+
+  async getAll(page, size, idDanhMuc, isActive, search) {
+    const { data, totalItems } = await TinTucRepository.getAll(
+      page,
+      size,
+      idDanhMuc,
+      isActive,
+      search
+    );
+    const pagination = createPagination(page, size, totalItems);
+    return { data, pagination };
+  },
+
+  async delete(id, currentUser) {
+    if (!id === null || id === undefined) {
+      throw new BaseError(400, "ID tin tức không được để trống");
+    }
+    const existing = await TinTucRepository.findById(id);
+    if (!existing) {
+      throw new BaseError(404, "Tin tức không tồn tại");
+    }
+    if (existing.is_active) {
+      throw new BaseError(400, "Chỉ có tin tức đang hoạt động mới có thể xóa");
+    }
+
+    let data = {
+      is_delete: true,
+      nguoi_cap_nhat: currentUser,
+      thoi_gian_cap_nhat: new Date().toISOString(),
+    };
+
+    await TinTucRepository.update(id, data);
+  },
+
+  async createTinTuc(
+    idDanhMuc,
+    tieuDe,
+    noiDung,
+    isActive,
+    tacGia,
+    file,
+    currentUser
+  ) {
+    tieuDe = capitalizeWords(tieuDe);
+    tacGia = tacGia ? capitalizeWords(tacGia) : tacGia;
+
+    const existingDanhMuc = await DanhMucTinTucRepository.findById(
+      idDanhMuc,
+      false
+    );
+    if (!existingDanhMuc) {
+      throw new BaseError(404, "Danh mục tin tức không tồn tại");
+    }
+
+    let data = {
+      id_danh_muc: idDanhMuc,
+      tieu_de: tieuDe,
+      noi_dung: noiDung,
+      is_active: isActive,
+      tac_gia: tacGia,
+      nguoi_tao: currentUser,
+    };
+
+    if (file && file.length > 0) {
+      data.url_anh_dai_dien = file[0].relativeUrl;
+    }
+    if (isActive === true) {
+      if (data.url_anh_dai_dien == null || data.url_anh_dai_dien === "") {
+        throw new BaseError(
+          400,
+          "Ảnh đại diện tin tức là bắt buộc để xuất bản tin tức"
+        );
+      }
+      // Gửi thông báo xuất bản tin tức
+
+      data.is_noti = true;
+    }
+    return await TinTucRepository.create(data);
+  },
+
+  async updateTinTucStatus(id, isActive, currentUser) {
+    if (id === null || id === undefined) {
+      throw new BaseError(400, "ID tin tức không được để trống");
+    }
+    const existing = await TinTucRepository.findById(id);
+    if (!existing || existing.is_delete) {
+      throw new BaseError(404, "Tin tức không tồn tại");
+    }
+    const data = {
+      is_active: isActive,
+      nguoi_cap_nhat: currentUser,
+      thoi_gian_cap_nhat: new Date().toISOString(),
+    };
+    return await TinTucRepository.update(id, data);
+  },
+
+  async getTinTucToView(id, ip, deviceInfo) {
+    if (id === null || id === undefined) {
+      throw new BaseError(400, "ID tin tức không được để trống");
+    }
+    if (deviceInfo === null || deviceInfo === undefined) {
+      throw new BaseError(400, "Thông tin thiết bị không được để trống");
+    }
+
+    const result = await TinTucRepository.getTinTucAndUpdateViewCount(
+      id,
+      ip,
+      deviceInfo
+    );
+    return result;
+  }
 
 };
 
