39afcc918a2652d81488fe501031f0a108f28343
Lê Nguyễn Minh Kha <lenguyenminhkha1606@gmail.com>
2025-11-30 20:32:02 +0700

    add api get report tin tuc, export

--- src/controllers/report.controller.js
+++ src/controllers/report.controller.js
@@ -1,63 +1,70 @@
 import ReportService from "../services/report.service.js";
 import { successResponse } from "../utils/response.util.js";
 
-
 const ReportController = {
-    async getBaoCaoTongHop(req, res) {
-        const { from, to } = req.query;
-        let data = await ReportService.getBaoCaoTongHop(from, to);
-        return successResponse(res, data, "Lấy báo cáo tổng hợp thành công");
-    },
+  async getReportPhanAnh(req, res) {
+    const { from, to, id_linh_vuc } = req.query;
+    let data = await ReportService.getReportPhanAnh(from, to, id_linh_vuc);
+    return successResponse(res, data, "Lấy báo cáo phản ánh thành công");
+  },
 
-    async exportBaoCaoTongHopExcel(req, res) {
-        const { from, to } = req.query;
-        const excelBuffer = await ReportService.exportBaoCaoTongHopExcel(from, to);
+  async getReportThuTuc(req, res) {
+    const { from, to } = req.query;
+    let data = await ReportService.getReportThuTuc(from, to);
+    return successResponse(res, data, "Lấy báo cáo thủ tục thành công");
+  },
 
-        res.setHeader("Content-Type", "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet");
-        res.setHeader("Content-Disposition", "attachment; filename=bao-cao-tong-hop.xlsx");
+  async getReportTinTuc(req, res) {
+    let { from, to } = req.query;
+    let data = await ReportService.getReportTinTuc(from, to);
+    return successResponse(res, data, "Lấy báo cáo tin tức thành công");
+  },
 
-        return res.send(excelBuffer);
-    },
+  async exportReportPhanAnh(req, res) {
+    const { from, to, idLinhVuc } = req.query;
+    const buffer = await ReportService.exportReportPhanAnh(
+      from,
+      to,
+      idLinhVuc
+    );
+    res.setHeader(
+      "Content-Type",
+      "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet"
+    );
+    res.setHeader(
+      "Content-Disposition",
+      "attachment; filename=bao_cao_phan_anh.xlsx"
+    );
+    return res.send(buffer);
+  },
 
-    async getBaoCaoLinhVuc(req, res) {
-        const { from, to } = req.query;
-        let data = await ReportService.getBaoCaoLinhVuc(from, to);
-        return successResponse(res, data, "Lấy báo cáo lĩnh vực thành công");
-    },
+  async exportReportThuTuc(req, res) {
+    const { from, to } = req.query;
+    const buffer = await ReportService.exportReportThuTuc(from, to);
+    res.setHeader(
+      "Content-Type",
+      "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet"
+    );
+    res.setHeader(
+      "Content-Disposition",
+      "attachment; filename=bao_cao_thu_tuc.xlsx"
+    );
+    return res.send(buffer);
+  },
 
-    async exportBaoCaoLinhVucExcel(req, res) {
-        const { from, to } = req.query;
-        const excelBuffer = await ReportService.exportBaoCaoLinhVucExcel(from, to);
-        res.setHeader("Content-Type", "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet");
-        res.setHeader("Content-Disposition", "attachment; filename=bao-cao-linh-vuc.xlsx");
-        return res.send(excelBuffer);
-    },
-
-    async getBaoCaoTrangThai(req, res) {
-        const { from, to } = req.query;
-        let data = await ReportService.getBaoCaoTrangThai(from, to);
-        return successResponse(res, data, "Lấy báo cáo đơn vị thành công");
-    },
-
-    async exportgetBaoCaoTrangThaiExcel(req, res) {
-        const { from, to } = req.query;
-        const excelBuffer = await ReportService.exportBaoCaoTrangThaiExcel(from, to);
-        res.setHeader("Content-Type", "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet");
-        res.setHeader("Content-Disposition", "attachment; filename=bao-cao-trang-thai.xlsx");
-        return res.send(excelBuffer);
-    },
-
-    async getReportPhanAnh(req, res) {
-        const { from, to, id_linh_vuc } = req.query;
-        let data = await ReportService.getReportPhanAnh(from, to, id_linh_vuc,);
-        return successResponse(res, data, "Lấy báo cáo phản ánh thành công");
-    },
-
-    async getReportThuTuc(req, res) {
-        const { from, to } = req.query;
-        let data = await ReportService.getReportThuTuc(from, to);
-        return successResponse(res, data, "Lấy báo cáo thủ tục thành công");
-    },
+  async exportReportTinTuc(req, res) {
+    const { from, to } = req.query;
+    const buffer = await ReportService.exportReportTinTuc(from, to);
+    res.setHeader(
+      "Content-Type",
+      "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet"
+    );
+    res.setHeader(
+      "Content-Disposition",
+      "attachment; filename=bao_cao_tin_tuc.xlsx"
+    );
+    return res.send(buffer);
+  }
 };
 
 export default ReportController;
--- src/routes/report.route.js
+++ src/routes/report.route.js
@@ -6,48 +6,6 @@ import { PERMISSION } from "../constants/permission.constant.js";
 const reportRouter = express.Router();
 
 reportRouter.get(
-  "/tong-hop",
-  authenticate,
-  authorize([PERMISSION.RPT_GET]),
-  ReportController.getBaoCaoTongHop
-);
-
-reportRouter.get(
-  "/tong-hop/export-excel",
-  authenticate,
-  authorize([PERMISSION.RPT_GET_EXCEL]),
-  ReportController.exportBaoCaoTongHopExcel
-);
-
-reportRouter.get(
-  "/linh-vuc",
-  authenticate,
-  authorize([PERMISSION.RPT_GET]),
-  ReportController.getBaoCaoLinhVuc
-);
-
-reportRouter.get(
-  "/linh-vuc/export-excel",
-  authenticate,
-  authorize([PERMISSION.RPT_GET_EXCEL]),
-  ReportController.exportBaoCaoLinhVucExcel
-);
-
-reportRouter.get(
-  "/trang-thai",
-  authenticate,
-  authorize([PERMISSION.RPT_GET]),
-  ReportController.getBaoCaoTrangThai
-);
-
-reportRouter.get(
-  "/trang-thai/export-excel",
-  authenticate,
-  authorize([PERMISSION.RPT_GET_EXCEL]),
-  ReportController.exportgetBaoCaoTrangThaiExcel
-);
-
-reportRouter.get(
   "/phan-anh",
   authenticate,
   authorize([PERMISSION.RPT_GET]),
@@ -59,6 +17,34 @@ reportRouter.get(
   authenticate,
   authorize([PERMISSION.RPT_GET]),
   ReportController.getReportThuTuc
+);
+
+reportRouter.get(
+  "/tin-tuc",
+  authenticate,
+  authorize([PERMISSION.RPT_GET]),
+  ReportController.getReportTinTuc
+);
+
+reportRouter.get(
+  "/phan-anh/export",
+  authenticate,
+  authorize([PERMISSION.RPT_GET_EXCEL]),
+  ReportController.exportReportPhanAnh
+);
+
+reportRouter.get(
+  "/thu-tuc/export",
+  authenticate,
+  authorize([PERMISSION.RPT_GET_EXCEL]),
+  ReportController.exportReportThuTuc
+);
+
+reportRouter.get(
+  "/tin-tuc/export",
+  authenticate,
+  authorize([PERMISSION.RPT_GET]),
+  ReportController.exportReportTinTuc
 );
 
 export default reportRouter;
