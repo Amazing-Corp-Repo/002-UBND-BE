f5411b381b4c9f0048bd2e26d28cdcbd08e94c44
Lê Nguyễn Minh Kha <lenguyenminhkha1606@gmail.com>
2025-12-05 22:25:56 +0700

    update permission

--- src/services/permission.service.js
+++ src/services/permission.service.js
@@ -3,14 +3,20 @@ import {
   PERMISSION,
   PERMISSION_CATEGORIES,
   PERMISSION_DESC,
+  PERMISSION_TYPE,
 } from "../constants/permission.constant.js";
 
 const PermissionService = {
   async getAllPermissions(search, danhMuc) {
-    let data = Object.values(PERMISSION).map((code) => ({
-      code,
-      description: PERMISSION_DESC[code] || null,
-    }));
+    let data = Object.values(PERMISSION).map((code) => {
+      const parts = code.split("_");
+      const type = parts.slice(1).join("_");
+      return {
+        code,
+        description: PERMISSION_DESC[code] || null,
+        type,
+      };
+    });
 
     if (danhMuc) {
       data = data.filter((item) => item.code.startsWith(`${danhMuc}_`));
@@ -29,13 +35,12 @@ const PermissionService = {
 
       if (!grouped[prefix]) grouped[prefix] = [];
 
-      grouped[prefix].push({
-        code: item.code,
-        description: item.description,
-      });
+      grouped[prefix].push(item);
     });
-
-    return grouped;
+    let cate = PERMISSION_CATEGORIES;
+    let type = PERMISSION_TYPE;
+    
+    return { grouped, cate, type };
   },
   async syncPermissions() {
     return await PermissionRepository.syncPermissions();
